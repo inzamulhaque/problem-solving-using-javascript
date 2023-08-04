2415a12df3cb7c98a5ef7852ee4ef23d05af2756
inzamulhaque <mdihalif@gmail.com>
2023-08-05 00:01:07 +0600

    Calculate the Power

---
+++ CalculateThePower.js
@@ -0,0 +1,7 @@
+const calculatePower = (num, pow) => {
+  return Math.pow(num, pow);
+};
+
+console.log(calculatePower(2, 3));
+console.log(calculatePower(5, 2));
+console.log(calculatePower(3, 4));
